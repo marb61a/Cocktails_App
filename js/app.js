acb356acf2fdb619bba195e98e286f6470f3405a
martin byrne <marb61a@gmail.com>
2018-05-03 16:01:22 +0000

    added initial js files and category.html

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Instantiate classes
+const ui = new UI(),
+      cocktail = new CocktailAPI(),
+      cocktailDB = new CocktailDB();
+
+// Create the Event Listeners
+function eventListeners(){
+    // Document Ready
+     document.addEventListener('DOMContentLoaded', documentReady);
+}
+
+eventListeners();
+
+// Document ready function
+function documentReady(){
+    
+}
