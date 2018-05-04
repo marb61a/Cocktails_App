a947d87f0f951f91477d7636bdae58179191f384
martin byrne <marb61a@gmail.com>
2018-05-04 10:49:17 +0000

    added save and get methods to cocktaildb.js

--- js/CocktailDB.js
+++ js/CocktailDB.js
@@ -1,3 +1,25 @@
 class CocktailDB {
+    // Save recipes into local storage
+    saveIntoDB(drink){
+        const drinks = this.getFromDB();
+        
+        drinks.push(drink);
+        
+        // Add the new array into localstorage
+        localstorage.setItem('drinks', JSON.stringify(drinks));
+    }
     
+    // Return recipes from localstorage
+    getFromDB(){
+        let drinks;
+        
+        // Check from localstorage
+        if(localStorage.getItem('drinks') === null){
+            drinks = [];
+        } else {
+            drinks = JSON.parse( localStorage.getItem('drinks') );
+        }
+        
+        return drinks;
+    }
 }
