59f66a5c2ff9a5bef69abb7995ca7c0b502a8245
martin byrne <marb61a@gmail.com>
2018-05-07 10:27:43 +0000

    added initial filter-alcohol.html file and added displayCategories function to UI.js

--- js/UI.js
+++ js/UI.js
@@ -1,3 +1,22 @@
 class UI {
+    // Display the drinks categories
+    displayCategories(){
+        const categoryList = cocktail.getCategories()
+            .then(categories => {
+                // Append a first option without value
+                const firstOption = document.createElement('option');
+                firstOption.textContent = '- Select -';
+                firstOption.value = '';
+                document.querySelector('#search').appendChild(firstOption);
+                
+                // Append into the select
+                catList.forEach(category => {
+                    const option = document.createElement('option');
+                    option.textContent = category.strCategory;
+                    option.value = category.strCategory.split(' ').join('_');
+                    document.querySelector('#search').appendChild(option);
+                });
+            });
+    }
     
 }
