9014a9aefa1ee9e5d748cc20a81b3fef094a20f0
martin byrne <marb61a@gmail.com>
2018-05-08 10:47:33 +0000

    added displayDrinks function to UI.js

--- js/UI.js
+++ js/UI.js
@@ -19,4 +19,36 @@ class UI {
             });
     }
     
+    // Display cocktails without ingredients
+    displayDrinks(drinks){
+        // Show results
+        const resultsWrapper = document.querySelector('results-wrapper');
+        resultsWrapper.style.display = 'block';
+        
+        // Insert the results
+        const resultsDiv = document.querySelector('#results');
+        
+        // Loop through the drinks
+        drinks.forEach(drink => {
+            resultsDiv.innerHTML += `
+                <div class="col-md-4">
+                    <div class="card my-3">
+                        <button type="button" data-id="${drink.idDrink}" class="favorite-btn btn btn-outline-info">
+                            +
+                        </button>
+                        <img class="card-img-top" src="http://${drink.strDrinkThumb}" alt="http://${drink.strDrink}">
+                        <div class="card-body">
+                            <h2 class="card-title text-center">${drink.strDrink}</h2>
+                            <a data-target="#recipe" class="btn btn-success get-recipe" href="#" data-toggle="modal" data-id="${drink.idDrink}">
+                                Get Recipe
+                            </a>
+                        </div>
+                    </div>
+                <div>
+            `;
+        });
+        
+        this.isFavorite();
+    }
+    
 }
