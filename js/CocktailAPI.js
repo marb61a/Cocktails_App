148eecff0e37bad51a05ec7c3c559c37b121a80c
martin byrne <marb61a@gmail.com>
2018-05-08 11:03:59 +0000

    added getDinksByName and getDrinksByIngredient functions to api

--- js/CocktailAPI.js
+++ js/CocktailAPI.js
@@ -1,3 +1,27 @@
 class CocktailAPI {
+    // Get recipe by name
+    async getDrinksByName(name){
+        // Search by name 
+        const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        
+        // This returns a JSON response
+        const cocktails = await apiResponse.json();
+        
+        return {
+            cocktails    
+        };
+    }
     
+    // Get recipes by ingredient
+    async getDrinksByIngredient(ingredient){
+        // Search by ingredient
+        const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+        
+        // Await response the return JSON
+        const cocktails = await apiResponse.json();
+        
+        return {
+            cocktails    
+        };
+    }
 }
