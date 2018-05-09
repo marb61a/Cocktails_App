115e303a0cb627d1d880b8b8161db267b7897781
martin byrne <marb61a@gmail.com>
2018-05-09 11:31:48 +0000

    added getCategories getDrinksByCategory anf getDrinksByAlcohol functions to API.js file

--- js/CocktailAPI.js
+++ js/CocktailAPI.js
@@ -24,4 +24,41 @@ class CocktailAPI {
             cocktails    
         };
     }
+    
+    // Retrieves all categories from the API
+    async getCategories(){
+        const apiResponse = await fetch('http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+        
+         // Await response the return JSON
+        const cocktails = await apiResponse.json();
+        
+        return {
+            cocktails    
+        };
+    }
+    
+    // Get Drinks By Category
+     async getDrinksByCategory( category ) {
+          // Search by Category
+          const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
+          // Wait for response then return JSON
+          const cocktails = await apiResponse.json();
+
+          return {
+               cocktails
+          };
+     }
+
+     // Get alcohol or non alcohol drinks
+     async getDrinksByAlcohol( term ) {
+          // Search by Category
+          const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${term}`);
+          
+          // Wait for response then return JSON
+          const cocktails = await apiResponse.json();
+
+          return {
+               cocktails
+          };
+     }
 }
