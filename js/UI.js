44e8540c9c6a877858d021fd0167f8f3fef089b6
marb61a <marb61a@gmail.com>
2018-05-10 13:15:22 +0100

    troubleshooting bugs

--- js/UI.js
+++ js/UI.js
@@ -3,28 +3,28 @@ class UI {
     displayCategories(){
         const categoryList = cocktail.getCategories()
             .then(categories => {
-                const catList = categories.categories.drinks;
-                
+            const catList = categories.categories.drinks;
+
                 // Append a first option without value
                 const firstOption = document.createElement('option');
                 firstOption.textContent = '- Select -';
                 firstOption.value = '';
                 document.querySelector('#search').appendChild(firstOption);
-                
-                // Append into the select
+
+                 // Append into the Select
                 catList.forEach(category => {
-                    const option = document.createElement('option');
-                    option.textContent = category.strCategory;
-                    option.value = category.strCategory.split(' ').join('_');
-                    document.querySelector('#search').appendChild(option);
-                });
-            });
+                     const option = document.createElement('option');
+                     option.textContent = category.strCategory;
+                     option.value = category.strCategory.split(' ').join('_');
+                     document.querySelector('#search').appendChild(option);
+                })
+            })
     }
     
     // Display cocktails without ingredients
     displayDrinks(drinks){
         // Show results
-        const resultsWrapper = document.querySelector('results-wrapper');
+        const resultsWrapper = document.querySelector('.results-wrapper');
         resultsWrapper.style.display = 'block';
         
         // Insert the results
@@ -55,7 +55,7 @@ class UI {
     
     // Display cocktails with ingredients
     displayDrinksWithIngredients(drinks){
-        const resultsWrapper = document.querySelector('results-wrapper');
+        const resultsWrapper = document.querySelector('.results-wrapper');
         resultsWrapper.style.display = 'block';
         
         // Insert the results
