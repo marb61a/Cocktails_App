08bf5505e0fdfe39a5dfb21ccf153f84e5ebfdca
martin byrne <marb61a@gmail.com>
2018-05-10 10:56:12 +0000

    added modal and foorter to category.html

--- js/CocktailAPI.js
+++ js/CocktailAPI.js
@@ -2,7 +2,7 @@ class CocktailAPI {
     // Get recipe by name
     async getDrinksByName(name){
         // Search by name 
-        const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
         
         // This returns a JSON response
         const cocktails = await apiResponse.json();
@@ -15,7 +15,7 @@ class CocktailAPI {
     // Get recipes by ingredient
     async getDrinksByIngredient(ingredient){
         // Search by ingredient
-        const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+        const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
         
         // Await response the return JSON
         const cocktails = await apiResponse.json();
@@ -27,7 +27,7 @@ class CocktailAPI {
     
     // Retrieves all categories from the API
     async getCategories(){
-        const apiResponse = await fetch('http://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+        const apiResponse = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
         
          // Await response the return JSON
         const cocktails = await apiResponse.json();
@@ -40,7 +40,7 @@ class CocktailAPI {
     // Get Drinks By Category
      async getDrinksByCategory(category){
           // Search by Category
-          const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
+          const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
           // Wait for response then return JSON
           const cocktails = await apiResponse.json();
 
@@ -52,7 +52,7 @@ class CocktailAPI {
      // Get alcohol or non alcohol drinks
      async getDrinksByAlcohol(term){
           // Search by Category
-          const apiResponse = await fetch(`http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${term}`);
+          const apiResponse = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${term}`);
           
           // Wait for response then return JSON
           const cocktails = await apiResponse.json();
